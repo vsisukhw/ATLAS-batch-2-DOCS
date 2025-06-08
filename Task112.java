835de14b3f5e5c962deee30d1a9443a0e0f74178
Sukhwinder Singh <sainisukhwinder1908@gmail.com>
2025-06-08 23:06:47 +0530

    files for task 112 added

---
+++ Task112.java
@@ -0,0 +1,10 @@
+
+//driver class
+public class Task112{
+    public static void main(String[] args){
+        System.out.println("Driver class called");
+        Superclass1 sobj = new Superclass1();
+        sobj.superMethod();
+        System.out.println("Driver class ended");
+    }
+}
