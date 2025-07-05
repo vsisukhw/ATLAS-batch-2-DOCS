c36baabdf5946cb107b150ea76c558023584ba17
Sukhwinder Singh <sainisukhwinder1908@gmail.com>
2025-07-05 11:39:46 +0530

    node  for a tree and include a constructor

---
+++ Day 15 Files/Task1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class TreeNode{
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int a)
+    {
+        data=a;
+        left=null;
+        right=null;
+    }
+}
+
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
