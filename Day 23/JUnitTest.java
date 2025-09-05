3c5d1b9669f4634e9c132419ddb21d8eac3ea81c
Sukhwinder Singh <sainisukhwinder1908@gmail.com>
2025-09-05 22:20:17 +0530

    added

---
+++ Day 23/JUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JUnitTest {
+    @Test
+    public void add(){
+        int sum=20;
+        assertEquals(20,sum);
+    }
+}
